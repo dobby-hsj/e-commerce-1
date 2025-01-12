35c5fa12c7446dd5ce812dd34b021f12a9d1325d
dobby <hsjoo@aitrics.com>
2025-01-12 20:55:34 +0900

    fix : li list 제거

--- frontend/components/admin/layouts/SideBar.tsx
+++ frontend/components/admin/layouts/SideBar.tsx
@@ -18,18 +18,15 @@ export const SideBar = () => {
   return (
     <aside className='fixed top-0 z-40 w-64 pt-20 h-screen hidden sm:block border'>
       <div className='h-full px-3 pb-4 overflow-y-auto bg-white '>
-        <ul>
           {sideBarList.map((item) => (
-            <li>
               <Link 
+                key={item.id}
                 href={item.href}
                 className='block cursor-pointer hover:bg-gray-200 p-2 rounded-sm text-gray-700'
               >
                 {item.label}
               </Link>
-            </li>
           ))}
-        </ul>
       </div>
     </aside>
   )
